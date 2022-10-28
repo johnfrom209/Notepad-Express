9ae3c17b5862e7962d7627f6af6debf780302cf2
Jonathan Moreno <johnfrom209@gmail.com>
2022-10-27 17:44:53 -0700

    changed port to PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const uniqid = require('uniqid');
 let textSaved = require('./db/db.json');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
